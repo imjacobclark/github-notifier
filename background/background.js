ba7733774e65819a3872006fbc0a8b7c3465fd60
Jacob Clark <jacob.jh.clark@googlemail.com>
2015-11-09 15:06:31 +0000

    If not initial run and rich notifications

--- background/background.js
+++ background/background.js
@@ -2,7 +2,17 @@ var GitHubNotifier = function(){
 	this.currentPullRequests = [];
 }
 
-GitHubNotifier.prototype.getData = function(){
+chrome.notifications.onButtonClicked.addListener(function(notifId, btnIdx) {
+	var notificationInformation = notifId.split(':');
+
+	chrome.tabs.create(
+		{
+			url: 'http://github.com/' + notificationInformation[0] + '/' + notificationInformation[1] + '/pulls'
+		}
+	);
+});
+
+GitHubNotifier.prototype.getData = function(initialRun){
 	chrome.storage.sync.get("data", function (obj) {
 		obj.data.forEach(function(project){
 			if(project.org !== undefined && project.repo !== undefined){
@@ -14,7 +24,7 @@ GitHubNotifier.prototype.getData = function(){
 
 				data = request.onload = function() {
 				  if (this.status >= 200 && this.status < 400) {
-				  	_this.parseData(this.response, project.org, project.repo);
+				  	_this.parseData(this.response, project.org, project.repo, initialRun);
 				  }
 				};
 
@@ -24,35 +34,43 @@ GitHubNotifier.prototype.getData = function(){
 	}.bind(this));
 }
 
-GitHubNotifier.prototype.parseData = function(resp, org, repo){
+GitHubNotifier.prototype.parseData = function(resp, org, repo, initialRun){
   var container = document.implementation.createHTMLDocument().documentElement;
   container.innerHTML = resp;
   var nodeList = container.querySelectorAll('.table-list-issues .js-issue-row .issue-title-link');
-
  	[].forEach.call(nodeList, function(div, i) {
-		if(this.currentPullRequests.length === 0){
-			this.currentPullRequests.length = 1;
-		}else{
-			if(this.currentPullRequests.indexOf(div.text.trim()) === -1){
-				chrome.notifications.create(div.text.trim(),
+		if(this.currentPullRequests.indexOf(div.text.trim()) === -1){
+			if(!initialRun){
+				chrome.notifications.create(org + ':' + repo  + ':' + div.text.trim(),
 				{
 						type:"basic",
 						title:org + '/' + repo,
-						message: div.text.trim(),
-						iconUrl:"../icons/512.png"
+						message: "New pull request opened \n\n" + div.text.trim() + "\n",
+						iconUrl:"../icons/512.png",
+						buttons: [
+							{
+				            	title: "View open pull requests"
+				        	}
+						]
 						}, function() {}
 				);
-			};
+			}
 
 			this.currentPullRequests.push(div.text.trim());
-		}
+		};
 	}.bind(this));
 }
 
 var ghprn = new GitHubNotifier();
 
-ghprn.getData();
+ghprn.getData(true);
 
 setInterval(function(){
-    ghprn.getData();
+	chrome.notifications.getAll(function(data){
+		for(var key in data) {
+			chrome.notifications.clear(key);
+		}
+	});
+
+    ghprn.getData(false);
 }, 5000);
