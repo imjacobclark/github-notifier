c6a2ae1f8963e898c19d7279d9aef3439a24c9ed
Jacob Clark <jacob.jh.clark@googlemail.com>
2016-02-06 01:17:46 +0000

    Error handling & UI improvements

--- background/background.js
+++ background/background.js
@@ -1,150 +1,162 @@
+"use strict";
+
 var GitHubNotifier = function(){
-	this.currentNotifications = [];
-	this.warmRepositories = [];
+    this.currentNotifications = [];
+    this.warmRepositories = [];
 }
 
 function XHRGetRequest(url){
-	return new Promise(function(resolve, reject){
-		var request = new XMLHttpRequest();
+    return new Promise(function(resolve, reject){
+        let request = new XMLHttpRequest();
 
-		request.open('GET', url, true);
+        request.open('GET', url, true);
 
-		var _this = this;
+        request.onload = function() {
+            if (this.status >= 200 && this.status < 400) {
+                resolve(this.response);
+            }
+            
+            reject(this)
+        }
 
-		data = request.onload = function() {
-			if (this.status >= 200 && this.status < 400) {
-				resolve(this.response);
-			}else{
-				reject(this.status)
-			}
-		}
-
-		request.send();
-	});
+        request.send();
+    });
 }
 
 function displayNotification(id, title, message, buttons){
-	chrome.notifications.create(id,
-	{
-			type: "basic",
-			title: title,
-			message: message,
-			iconUrl: "../icons/512.png",
-			buttons: buttons
-			}, function() {}
-	);
+    chrome.notifications.create(
+        id,
+        {
+            type: "basic",
+            title: title,
+            message: message,
+            iconUrl: "../icons/512.png",
+            buttons: buttons
+        }, 
+        function() {}
+    );
 }
 
-chrome.notifications.onButtonClicked.addListener(function(notifId, btnIdx) {
-	if(notifId === 'notloggedin'){
-		chrome.tabs.create(
-			{
-				url: 'https://github.com/login'
-			}
-		);
-	}else{
-		var notificationInformation = notifId.split(':');
+chrome.notifications.onButtonClicked.addListener(
+    function(notifId, btnIdx) {
+        if (notifId === 'notloggedin') {
+            chrome.tabs.create(
+                {
+                    url: 'https://github.com/login'
+                }
+            );
+        } else if(notifId === '404'){
 
-		chrome.tabs.create(
-			{
-				url: 'http://github.com' + notificationInformation[3]
-			}
-		);
-	}
-});
+        } else {
+            let notificationInformation = notifId.split(':');
+            
+            chrome.tabs.create(
+                {
+                    url: 'http://github.com' + notificationInformation[3]
+                }
+            );
+        }
+    }
+);
 
 GitHubNotifier.prototype.getData = function(initialRun){
-	chrome.storage.sync.get("data", (obj) => {
-		obj.data.forEach((project) => {
-			var isCold = this.warmRepositories.indexOf(project.org + ":" + project.repo) === -1;
+    chrome.storage.sync.get("data", (obj) => {
+        obj.data.forEach((project) => {
+            let isCold = this.warmRepositories.indexOf(project.org + ":" + project.repo) === -1;
 
-			if(isCold){
-				initialRun = true;
-			}
+            if(isCold){
+                initialRun = true;
+            }
 
-			if(project.org !== undefined && project.repo !== undefined){
-				XHRGetRequest('http://github.com/' + project.org + '/' + project.repo + '/pulls').then((data) => {
-					this.parseData(data, project.org, project.repo, initialRun, 'pull');
-					return XHRGetRequest('http://github.com/' + project.org + '/' + project.repo + '/issues');
-				}).then((data) => {
-					this.parseData(data, project.org, project.repo, initialRun, 'issue');
-					return XHRGetRequest('http://github.com/' + project.org + '/' + project.repo + '/releases');
-				}).then((data) => {
-					this.parseData(data, project.org, project.repo, initialRun, 'release');
-				}).catch((e) => {
-					displayNotification(
-						'notloggedin',
-						'GitHub Notifier - Error',
-						"You don't appear signed into GitHub! \n\n",
-						[{ title: "Sign in" }]
-					);
-				});
-			};
+            if(project.org !== undefined && project.repo !== undefined){
+                XHRGetRequest('http://github.com/' + project.org + '/' + project.repo + '/pulls').then((data) => {
+                    this.parseData(data, project.org, project.repo, initialRun, 'pull');
+                    return XHRGetRequest('http://github.com/' + project.org + '/' + project.repo + '/issues');
+                }).then((data) => {
+                    this.parseData(data, project.org, project.repo, initialRun, 'issue');
+                    return XHRGetRequest('http://github.com/' + project.org + '/' + project.repo + '/releases');
+                }).then((data) => {
+                    this.parseData(data, project.org, project.repo, initialRun, 'release');
+                }).catch((e) => {
+                    if(e.status == 404){
+                        displayNotification(
+                            '404',
+                            'GitHub Notifier - Error',
+                            "Error: " + e.status + " " + e.statusText + "\nFor: " + e.responseURL + "\n\nPlease verify the organisation/user and repo are correct."
+                        );
+                    }else{
+                        displayNotification(
+                            'notloggedin',
+                            'GitHub Notifier - Error',
+                            "You don't appear signed into GitHub! \n\n",
+                            [{ title: "Sign in" }]
+                        );
+                    }
+                   
+                });
+            };
 
-			if(isCold){
-				this.warmRepositories.push(project.org + ":" + project.repo);
-			}
-		})
-	});
+            if(isCold){
+                this.warmRepositories.push(project.org + ":" + project.repo);
+            }
+        });
+    });
 }
 
 GitHubNotifier.prototype.parseData = function(resp, org, repo, initialRun, type){
-	var container = document.implementation.createHTMLDocument().documentElement;
-	container.innerHTML = resp;
+    let container = document.implementation.createHTMLDocument().documentElement;
+    container.innerHTML = resp;
 
-	switch(type){
-		case 'pull':
-			var selectors = '.table-list-issues .js-issue-row .issue-title-link',
-			type = 'pull request'
-			break;
-		case 'issue':
-			var selectors = '.table-list-issues .js-issue-row .issue-title-link',
-				type = 'issue'
-			break;
-		case 'release':
-			var selectors = '.release-timeline .release-header .release-title a',
-				type = 'release'
-			break;
-		default:
-			var selectors = '.table-list-issues .js-issue-row .issue-title-link',
-			type = 'pull request'
-			break;
-	};
+    switch(type){
+        case 'pull':
+            var selectors = '.table-list-issues .js-issue-row .issue-title-link',
+            type = 'pull request'
+            break;
+        case 'issue':
+            var selectors = '.table-list-issues .js-issue-row .issue-title-link',
+            type = 'issue'
+            break;
+        case 'release':
+            var selectors = '.release-timeline .release-header .release-title a',
+            type = 'release'
+            break;
+        default:
+            var selectors = '.table-list-issues .js-issue-row .issue-title-link',
+            type = 'pull request'
+            break;
+    };
 
-	var notificationNodeList = container.querySelectorAll(selectors);
-	var notificationMetadataNodeList = container.querySelectorAll('.table-list-issues .js-issue-row  .tooltipped-s');
-	var notificationReleaseMetadataNodeList = container.querySelectorAll('.release-timeline .release-authorship a');
+	let notificationNodeList = container.querySelectorAll(selectors),
+        notificationMetadataNodeList = container.querySelectorAll('.table-list-issues .js-issue-row  .tooltipped-s'),
+        notificationReleaseMetadataNodeList = container.querySelectorAll('.release-timeline .release-authorship a');
 
-	[].forEach.call(notificationNodeList, (notification, i) => {
-			if(this.currentNotifications.indexOf(notification.text.trim()) === -1){
-				if(!initialRun){
-					if(type !== 'release'){
-						var notificationAuthor = notificationMetadataNodeList[i].text.trim();
-					}else{
-						var notificationAuthor = notificationReleaseMetadataNodeList[i].text.trim();
-					}
+    [].forEach.call(notificationNodeList, (notification, i) => {
+        if(this.currentNotifications.indexOf(notification.text.trim()) === -1){
+            if(!initialRun){
+                if(type !== 'release'){
+                    var notificationAuthor = notificationMetadataNodeList[i].text.trim();
+                }else{
+                    var notificationAuthor = notificationReleaseMetadataNodeList[i].text.trim();
+                }
 
-					var notificationTitle = notification.text.trim();
-					var notificationLink = notification.attributes['href'].value;
-					var message = notificationTitle + '\n\nAuthor: ' + notificationAuthor;
+                let notificationTitle = notification.text.trim(),
+                    notificationLink = notification.attributes['href'].value,
+                    message = notificationTitle + '\n\nAuthor: ' + notificationAuthor;
 
-					displayNotification(
-						org + ':' + repo  + ':' + notificationTitle.replace(':', '') + ':' + notificationLink,
-						org + '/' + repo + " - " + type,
-						message,
-						[{ title: "View " + type }]
-					);
-				}
+                displayNotification(
+                    org + ':' + repo  + ':' + notificationTitle.replace(':', '') + ':' + notificationLink,
+                    org + '/' + repo + " - " + type,
+                    message,
+                    [{ title: "View " + type }]
+                );
+            }
 
-				this.currentNotifications.push(notification.text.trim());
-			};
-	});
+            this.currentNotifications.push(notification.text.trim());
+        };
+    });
 }
 
 var ghprn = new GitHubNotifier();
-
 ghprn.getData(true);
 
-setInterval(() => {
-    ghprn.getData(false);
-}, 5000);
+setInterval(() => ghprn.getData(false), 5000);
--- client/options.js
+++ client/options.js
@@ -1,61 +1,63 @@
+"use strict";
+
 function populateProjectList(){
-	chrome.storage.sync.get("data", function (obj) {
-		if(obj.data === null || obj.data === undefined || obj.data.length === 0){
-			document.querySelector('.add-project__new-user').style.display = 'block'
-			document.querySelector('.add-project__been-here-before').style.display = 'none';
-		}else{
-			document.querySelector('.add-project__been-here-before').style.display = 'block';
-			document.querySelector('.add-project__new-user').style.display = 'none'
-		}
+    chrome.storage.sync.get("data", function (obj) {
+        if(obj.data === null || obj.data === undefined || obj.data.length === 0){
+            document.querySelector('.add-project__new-user').style.display = 'block'
+            document.querySelector('.add-project__been-here-before').style.display = 'none';
+        }else{
+            document.querySelector('.add-project__been-here-before').style.display = 'block';
+            document.querySelector('.add-project__new-user').style.display = 'none'
+        }
 
-		var projectForms = document.querySelector('.add-project__forms');
-		projectForms.innerHTML = "";
+        let projectForms = document.querySelector('.add-project__forms');
+        projectForms.innerHTML = "";
 
-		obj.data.forEach(function(project){
-			projectForms.insertAdjacentHTML("beforeend", '\
-			<div class="add-project__form">\
-				<input type="text" value="' + project.org + '" placeholder="Organisation" class="org">\
-				<input type="text" value="' + project.repo + '" placeholder="Repo" class="repo">\
-			</div>');
-		});
-	});
+        obj.data.forEach(function(project){
+            projectForms.insertAdjacentHTML("beforeend", '\
+                <div class="add-project__form">\
+                <input type="text" value="' + project.org + '" placeholder="Organisation/User" class="org">\
+                <input type="text" value="' + project.repo + '" placeholder="Repo" class="repo">\
+            </div>');
+        });
+    });
 }
 
 function saveProjects(){
-	var projects = document.querySelectorAll('.add-project__form');
-	var projectStorageArr = [];
+    let projects = document.querySelectorAll('.add-project__form'),
+        projectStorageArr = [];
 
-	for(var i = 0; i < projects.length; i++){
-		projectStorageArr.push({
-			org: projects[i].querySelector(".org").value,
-			repo: projects[i].querySelector(".repo").value
-		});
-	}
+    for(let i = 0; i < projects.length; i++){
+        projectStorageArr.push({
+            org: projects[i].querySelector(".org").value,
+            repo: projects[i].querySelector(".repo").value
+        });
+    }
 
-	chrome.storage.sync.set(
-		{
-			'data': projectStorageArr
-		}
-	);
+    chrome.storage.sync.set(
+        {
+            'data': projectStorageArr
+        }
+    );
 }
 
 document.querySelector(".add-project__save-bttn").addEventListener("click", function(e){
-	saveProjects();
-	e.preventDefault();
+    saveProjects();
+    e.preventDefault();
 });
 
 document.querySelector('.add-project__watch-new-bttn').addEventListener("click", function(e){
-	saveProjects();
+    saveProjects();
 
-	var projectForms = document.querySelector('.add-project__forms');
+    let projectForms = document.querySelector('.add-project__forms');
 
-	projectForms.insertAdjacentHTML("beforeend", '\
-	<div class="add-project__form">\
-		<input type="text" placeholder="Organisation" class="org">\
-		<input type="text" placeholder="Repo" class="repo">\
-	</div>');
+    projectForms.insertAdjacentHTML("beforeend", '\
+        <div class="add-project__form">\
+        <input type="text" placeholder="Organisation/User" class="org" title="This is the text of the tooltip">\
+        <input type="text" placeholder="Repo" class="repo">\
+    </div>');
 
-	e.preventDefault();
+    e.preventDefault();
 });
 
 populateProjectList();
